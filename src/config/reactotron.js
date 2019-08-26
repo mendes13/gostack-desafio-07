c9b5fe799dccb6e90168a32bb5697ebd6d543377
Gustavo Mendes <gustavomendes.medeiros@gmail.com>
2019-08-26 17:47:49 -0300

    Add react-native-config and fixed reactotron

--- src/config/reactotron.js
+++ src/config/reactotron.js
@@ -1,13 +1,14 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import ReactotronSaga from 'reactotron-redux-saga';
+import Config from 'react-native-config';
 
 if (__DEV__) {
   const tron = Reactotron.configure()
     .useReactNative()
     .use(reactotronRedux())
     .use(ReactotronSaga())
-    .connect();
+    .connect({ host: '' });
 
   console.tron = tron;
   tron.clear();
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 
 const api = axios.create({
-  baseURL: 'http://10.0.3.2:5000',
+  baseURL: `http://${Config.APP_HOST}:5000`,
 });
 
 export default api;
